5b1e5149bcccb4225a6881bfadf72e0027053fc6
ninesun <zhiyitec@qq.com>
2020-01-18 23:05:57 +0800

    新增生产环境与测试环境

--- 后端/pms/src/main/resources/static/js/config.js
+++ 后端/pms/src/main/resources/static/js/config.js
@@ -3,5 +3,5 @@
 // var alltime = 600;
 // var host = "http://192.168.43.209"
 
-var host = "http://123.57.24.75:8081/duoweike";
-//  var host="http://localhost:8081/duoweike";
+// var host = "http://123.57.24.75:8081/duoweike";
+ var host="http://localhost:8081/duoweike";
